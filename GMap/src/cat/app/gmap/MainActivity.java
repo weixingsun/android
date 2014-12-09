1069441ecfaafb0ec14b3a4e65bf09be3c30c6c0
weixingsun <weixing.sun@gmail.com>
2014-12-09 13:31:23 +1300

    create a google map app with latest API v2

---
+++ GMap/src/cat/app/gmap/MainActivity.java
@@ -0,0 +1,34 @@
+package cat.app.gmap;
+
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+public class MainActivity extends android.app.Activity {
+
+	GoogleMap map;
+    //Sydney (-33.867, 151.206)
+    //Beijing (39.915291, 116.396860)
+    //Shanghai(31.192609, 121.431577)
+	//Christchurch(-43.5320544,172.6362254)
+	
+    @SuppressLint("NewApi") @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        String comments1 = "The capital city of China.";
+        LatLng position = new LatLng(39.915291, 116.396860);
+        position = new LatLng(-43.5320544,172.6362254);
+        marker(position,comments1);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 13));
+    }
+    public void marker(LatLng point,String comments){
+    	map.setMyLocationEnabled(true);
+        map.addMarker(new MarkerOptions()
+        .title("Beijing")
+        .snippet(comments)
+        .position(point));
+    }
+}
