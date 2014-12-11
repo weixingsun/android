8471077ae930d06e33c1664ace6bca7c01b21872
weixingsun <weixing.sun@gmail.com>
2014-12-11 16:35:58 +1300

    move map operations into GMap class

--- GMap/src/cat/app/gmap/GMap.java
+++ GMap/src/cat/app/gmap/GMap.java
@@ -1,5 +1,41 @@
 package cat.app.gmap;
 
-public class GMap {
+import java.util.ArrayList;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class GMap {
+	GoogleMap map;
+	ArrayList<LatLng> markerPoints=new ArrayList<LatLng>();
+	@SuppressLint("NewApi") 
+	public void init(Activity activity){
+		map = ((MapFragment) activity.getFragmentManager().findFragmentById(R.id.map)).getMap();
+	}
+
+    public void addMarker(LatLng point,String comments){
+    	map.setMyLocationEnabled(true);
+    	map.getUiSettings().setCompassEnabled(true);
+    	map.setBuildingsEnabled(true);
+        map.addMarker(new MarkerOptions()
+        .title("Christchurch")
+        .snippet(comments)
+        .position(point));
+    }
+    public void move(LatLng point){
+    	map.moveCamera(CameraUpdateFactory.newLatLngZoom(point, 13));
+    }
+    private void changeMapType(){
+    	if(map.getMapType()==GoogleMap.MAP_TYPE_NORMAL){
+    		map.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+    	}else if(map.getMapType()==GoogleMap.MAP_TYPE_SATELLITE){
+    		map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+    	}
+    }
 }
--- GMap/src/cat/app/gmap/MainActivity.java
+++ GMap/src/cat/app/gmap/MainActivity.java
@@ -7,28 +7,16 @@ import android.os.Bundle;
 
 public class MainActivity extends android.app.Activity {
 
-	GoogleMap map;
-    //Sydney (-33.867, 151.206)
-    //Beijing (39.915291, 116.396860)
-    //Shanghai(31.192609, 121.431577)
-	//Christchurch(-43.5320544,172.6362254)
+	GMap gMap;
 	
-    @SuppressLint("NewApi") @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
         String comments1 = "A beautiful garden city";
-        LatLng position = new LatLng(39.915291, 116.396860);
-        position = new LatLng(-43.5320544,172.6362254);
-        marker(position,comments1);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 13));
+        LatLng position = Position.findLatLng("CHC");
+        gMap.addMarker(position,comments1);
+        gMap.move(position);
     }
-    public void marker(LatLng point,String comments){
-    	map.setMyLocationEnabled(true);
-        map.addMarker(new MarkerOptions()
-        .title("Christchurch")
-        .snippet(comments)
-        .position(point));
-    }
+    
+    
 }
