a45f503d5fbd6afba05eba34cc493308e775d221
weixingsun <weixing.sun@gmail.com>
2015-05-26 19:44:08 +1200

    change to maneuver types for graphhopper 0.5

--- SmartPark/src/wsn/park/navi/GraphHopperOfflineRoadManager.java
+++ SmartPark/src/wsn/park/navi/GraphHopperOfflineRoadManager.java
@@ -54,19 +54,35 @@ public class GraphHopperOfflineRoadManager extends RoadManager {
 	static final HashMap<Integer, Integer> MANEUVERS;
 
 	private static final String tag = GraphHopperOfflineRoadManager.class.getSimpleName();
+	//convert Graphhopper maneuver to MapQuest
 	static {
 		MANEUVERS = new HashMap<Integer, Integer>();
-		MANEUVERS.put(0, 1); //Continue
-		MANEUVERS.put(1, 6); //Slight right
-		MANEUVERS.put(2, 7); //Right
-		MANEUVERS.put(3, 8); //Sharp right
-		MANEUVERS.put(-3, 5); //Sharp left
-		MANEUVERS.put(-2, 4); //Left
-		MANEUVERS.put(-1, 3); //Slight left
-		MANEUVERS.put(4, 24); //Arrived
-		MANEUVERS.put(5, 24); //Arrived at waypoint
+		MANEUVERS.put(-3, 5); //Sharp_Left(-3) = Sharp_Left(5)
+		MANEUVERS.put(-2, 4); //Left(-2) = Left(4)
+		MANEUVERS.put(-1, 3); //Slight_Left(-1) = Slight_Left(3)
+		MANEUVERS.put(0, 1);  //Continue(0) = Straight(1)
+		MANEUVERS.put(1, 6);  //Slight_Right(1) = Slight_Right(6)
+		MANEUVERS.put(2, 7);  //Right(2) = Right(7)
+		MANEUVERS.put(3, 8);  //Sharp_Right(3) = Sharp_Right(8)
+		MANEUVERS.put(4, 24); //Arrived(4) = DESTINATION(24)
+		//MANEUVERS.put(4, 25); //Arrived(4) = DESTINATION_LEFT(25)
+		//MANEUVERS.put(4, 26); //Arrived(4) = DESTINATION_LEFT(26)
+		MANEUVERS.put(6, 27); //UseRoundabout(6) = ROUNDABOUT1(27)
+		//MANEUVERS.put(6, 28); //UseRoundabout(6) = ROUNDABOUT2(28)
 	}
-	
+	/*
+	public static final int LEAVE_ROUNDABOUT = -6; // for future use
+    public static final int TURN_SHARP_LEFT = -3;
+    public static final int TURN_LEFT = -2;
+    public static final int TURN_SLIGHT_LEFT = -1;
+    public static final int CONTINUE_ON_STREET = 0;
+    public static final int TURN_SLIGHT_RIGHT = 1;
+    public static final int TURN_RIGHT = 2;
+    public static final int TURN_SHARP_RIGHT = 3;
+    public static final int FINISH = 4;				//
+    public static final int REACHED_VIA = 5; 		//?
+    public static final int USE_ROUNDABOUT = 6;		//
+ * */
 	/**
 	 * @param apiKey GraphHopper API key, mandatory to use the public GraphHopper service. 
 	 * @see <a href="http://graphhopper.com/#enterprise">GraphHopper</a> to obtain an API key. 
@@ -124,12 +140,14 @@ public class GraphHopperOfflineRoadManager extends RoadManager {
 			return road;
 		}
 		road.mRouteHigh = this.getHighPoints(pointList);
+
 		for(Instruction hint:hintList){
 			RoadNode node = new RoadNode();
 			node.mLocation = new GeoPoint(hint.getPoints().getLat(0),hint.getPoints().getLon(0));
 			node.mLength = hint.getDistance()/1000;
 			node.mDuration = hint.getTime()/1000;
 			node.mManeuverType = getManeuverCode(hint.getSign());
+			Log.e(tag, "Instruction.sign="+hint.getSign());
 			String ann = hint.getAnnotation().getImportance()+": "+hint.getAnnotation().getMessage();
 			node.mInstructions = hint.getName()+" , "+ann;
 			road.mNodes.add(node);
